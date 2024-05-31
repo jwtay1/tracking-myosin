06b3cbd11e3d8e85039753d564a89c93ebec8f72
Jian Tay <jian.tay@colorado.edu>
2024-05-31 14:20:38 -0600

    Updated analysis script to filter

--- runScript.m
+++ runScript.m
@@ -3,5 +3,5 @@ clc
 
 T = FreeMyosinTracker;
 
-processVideos(T, 'C:\Users\Jian Tay\OneDrive - UCB-O365\Shared\Share with Leinwand Lab\Myosin tracking\data\test', ...
-    'C:\Users\Jian Tay\OneDrive - UCB-O365\Shared\Share with Leinwand Lab\Myosin tracking\processed\20240524_test')
+processVideos(T, 'C:\Users\Jian Tay\OneDrive - UCB-O365\Shared\Share with Leinwand Lab\Myosin tracking\data\Movie_selections', ...
+    'C:\Users\Jian Tay\OneDrive - UCB-O365\Shared\Share with Leinwand Lab\Myosin tracking\processed\20240524')
